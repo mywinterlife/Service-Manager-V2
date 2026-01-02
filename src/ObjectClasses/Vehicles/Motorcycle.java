e5249b2f0a8b19dfdd64d92bbd110b7be1a84d18
mywinterlifegames-admin1 <mywinterlifegames@gmail.com>
2026-01-01 18:21:45 -0700

    Additional vehicle classes

--- src/ObjectClasses/Vehicles/Motorcycle.java
+++ src/ObjectClasses/Vehicles/Motorcycle.java
@@ -1,6 +1,19 @@
 package ObjectClasses.Vehicles;
 
-public class Motorcycle
-{
+import Enums.OilTypeEnum;
 
+public class Motorcycle extends Vehicle
+{
+    private int passengerCount = 2;
+    public int wheelCount = 2;
+
+    //Similar to Vehicle:
+    //Write an explicit constructor
+    //Add all required properties as parameters
+    //Assign local values to class properties!!! This initializes them with base values.
+    public Motorcycle(String make, String model, OilTypeEnum recommendedOilType, double oilCapacity, int passengerCount)
+    {
+        super(make, model, recommendedOilType,oilCapacity,2);
+        this.passengerCount = passengerCount;
+    }
 }
